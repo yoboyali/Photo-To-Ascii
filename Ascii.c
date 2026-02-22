5c19d8a9c155a8c67d776645ea675c509c24641a
yoboyali <alihamdyeltomy@gmail.com>
2026-02-22 14:03:50 +0300

    Added Assets for ReadMe

--- Ascii.c
+++ Ascii.c
@@ -29,7 +29,7 @@ void CreateImage(unsigned char *ImageData){
        if(PixelSize >= 4){unsigned char a = *Pixels++;}
        float Average = (r + g + b) / 3.0;
        int CharIndex = (int)((Average / 255) * CharacterLen - 1);
-       printf("\33[38;2;%d;%d;%dm%c" , r , g , b ,Characters[CharIndex]);
+       printf("\33[38;2;%d;%d;%dm%c]" , r , g , b ,Characters[CharIndex]);
      // putchar(Characters[CharIndex]);
        usleep(100);
       }
